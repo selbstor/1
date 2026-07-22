465d8e6f63f2a9857991eb3dbab657e3a1ff53c9
selbstor <selbstor@gmail.com>
2026-07-22 04:34:32 -0300

    Add files via upload

---
+++ pedidos/public/config.js
@@ -0,0 +1,5 @@
+// Configuração global do aplicativo
+// Você pode alterar o link abaixo diretamente para atualizar a planilha carregada pelo aplicativo
+window.APP_CONFIG = {
+  pedidos: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSbcOCbWT91wgXhKdgoZcxoFeartiLRXRzw-XFHdEXVT2gJ3LjCiS85WJzEweZAN8aUQI_10lZjl1Pe/pub?gid=180773339&single=true&output=csv"
+};
